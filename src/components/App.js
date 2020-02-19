0d66e4c3f96f61c6137f0ce6c621102188cbd72f
Samuel Gagliardi <Samuel.Gagliardi@cohu.com>
2020-02-19 10:44:47 -0600

    initial work

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import singleBoard from "./singleBoard.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>test paragraph</p>
+      <body>
+        <div>
+          <singleBoard></singleBoard>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/singleBoard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./singleBoard.css";
+
+class Triangle extends React.Component {
+  state = {};
+
+  render() {
+    return <div className="triangle"><p>test</p></div>;
+  }
+}
+
+export default Triangle;
